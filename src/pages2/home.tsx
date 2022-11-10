9eb5dacec116c570d48f21a7f56a1d70b0badebe
johnny-mh <romz1212@gmail.com>
2022-11-10 23:39:16 +0900

    feat: adding scene

--- src/pages2/home.tsx
+++ src/pages2/home.tsx
@@ -1,49 +1,56 @@
 import { TypeAnimation } from "react-type-animation";
 import tw from "twin.macro";
 import styled from "styled-components";
+import { Suspense } from "react";
+import Scene from "../3d/Scene";
 
 function Home() {
   return (
     <>
       <Header>
-        <div tw="max-w-6xl mx-auto">
-          <nav>
-            <ul className="menu">
-              <li>HOME</li>
-              <li>ABOUT</li>
-              <li>SKILL</li>
-              <li>EXPERIENCE</li>
-              <li>PROJECTS</li>
-            </ul>
-          </nav>
-        </div>
-        <section tw="h-[90vh] flex items-center max-w-6xl mx-auto">
-          <h1 tw="font-bold text-6xl leading-relaxed">
-            <div>
+        <div tw="absolute z-10 inset-0">
+          <div tw="max-w-6xl mx-auto">
+            <nav>
+              <ul className="menu">
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>SKILL</li>
+                <li>EXPERIENCE</li>
+                <li>PROJECTS</li>
+              </ul>
+            </nav>
+          </div>
+          <section tw="mt-[45vh] flex items-center max-w-6xl mx-auto">
+            <h1 tw="font-bold text-6xl leading-relaxed">
               <div>
-                안녕하세요!
-                <br /> 프론트엔드 개발자{" "}
-                <span tw="text-orange-400">전지현</span>
-                입니다. 🎉
+                <div>
+                  안녕하세요!
+                  <br /> 프론트엔드 개발자{" "}
+                  <span tw="text-orange-400">전지현</span>
+                  입니다. 🎉
+                </div>
+                <div tw="text-orange-400">
+                  <TypeAnimation
+                    sequence={[
+                      "책임감의 아이콘",
+                      3000,
+                      "기본기가 탄탄해요",
+                      3000,
+                      "울보에요 🥲",
+                      3000,
+                    ]}
+                    wrapper="span"
+                    repeat={Infinity}
+                    cursor={true}
+                  />
+                </div>
               </div>
-              <div tw="text-orange-400">
-                <TypeAnimation
-                  sequence={[
-                    "책임감의 아이콘",
-                    3000,
-                    "기본기가 탄탄해요",
-                    3000,
-                    "울보에요 🥲",
-                    3000,
-                  ]}
-                  wrapper="span"
-                  repeat={Infinity}
-                  cursor={true}
-                />
-              </div>
-            </div>
-          </h1>
-        </section>
+            </h1>
+          </section>
+        </div>
+        <Suspense fallback={null}>
+          <Scene />
+        </Suspense>
       </Header>
     </>
   );
