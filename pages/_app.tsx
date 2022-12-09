0b0bd914b3eb87fae37d9a926b017938ff12e4af
jihyun-jeon <jhplus13@naver.com>
2022-12-09 15:28:36 +0900

    Feat : scroll restoration 기능 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,6 +22,41 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [router.events]);
   // GA 설정 끝
 
+  // scroll restoration
+  useEffect(() => {
+    window.history.scrollRestoration = "auto";
+
+    const cacheScrollPositions: Array<[number, number]> = [];
+    let shouldScrollRestore: null | { x: number; y: number };
+
+    router.events.on("routeChangeStart", () => {
+      cacheScrollPositions.push([window.scrollX, window.scrollY]);
+    });
+
+    router.events.on("routeChangeComplete", () => {
+      if (shouldScrollRestore) {
+        const { x, y } = shouldScrollRestore;
+        window.scrollTo(x, y);
+        shouldScrollRestore = null;
+      }
+      window.history.scrollRestoration = "auto";
+    });
+
+    router.beforePopState(() => {
+      if (cacheScrollPositions.length > 0) {
+        const scrollPosition = cacheScrollPositions.pop();
+        if (scrollPosition) {
+          shouldScrollRestore = {
+            x: scrollPosition[0],
+            y: scrollPosition[1],
+          };
+        }
+      }
+      window.history.scrollRestoration = "manual";
+      return true;
+    });
+  }, []);
+
   return (
     <>
       {/* GA 설정 시작 */}
